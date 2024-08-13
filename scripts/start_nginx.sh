e239b39fc260e2073dd0c0ea829adf6c00d03684
root <root@ip-172-31-24-178.ec2.internal>
2024-08-13 14:31:23 +0000

    abc

--- scripts/start_nginx.sh
+++ scripts/start_nginx.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 # Restart NGINX to apply any new configuration or to serve the new files
-sudo service restart nginx
+# sudo service restart nginx
+sudo service nginx restart
 echo "nginx started"
 
