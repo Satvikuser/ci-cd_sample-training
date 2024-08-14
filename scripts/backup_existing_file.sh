8baa527f675d5bde5cbdd7a7016e590adefc6288
Satvikuser <133895235+Satvikuser@users.noreply.github.com>
2024-08-14 16:43:40 +0530

    Delete scripts/backup_existing_file.sh

--- scripts/backup_existing_file.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-sudo apt-get update
-sudo apt-get install -y nginx 
-# Check if the file exists
-#if [ -f /var/www/html/index.html ]; then
-  # Backup the existing file
-#  cp /var/www/html/index.html /var/www/html/index.html.bak
-#fi
-echo "installed succesfully"
